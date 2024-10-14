14b15ab60609fbab00244c6286162c203645e954
reaksa1507 <longreak3@gmail.com>
2024-10-14 11:09:04 +0700

    add data type

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 100;
+            float pi = 3.14f;
+            double amount = 10000d;
+            string firstName = "ly";
+            string lastName = "ratana";
+
+            int number2 = 500;
+            int result = number + number2;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
